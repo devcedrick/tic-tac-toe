0520cf66104af555e1c3676da121fddcea967410
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2025-09-14 11:51:39 +0800

    Displays game history statistics.
    Adds a component to display the game history statistics, including X wins, O wins, draws, and total games played.
    
    Implements client-side rendering to avoid hydration issues.

--- components/layout/GameHistory.tsx
+++ components/layout/GameHistory.tsx
@@ -1,17 +1,31 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
 import GameStats from '../ui/GameStats'
 import GameStatsCard from '../ui/GameStatsCard'
+import { useGameContext } from '@/hooks/useGameContext'
 
 const GameHistory = () => {
+  const { gameStats } = useGameContext();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    <div className='bg-white w-max h-max p-10 rounded-2xl flex flex-col gap-5 items-center'>
+    <div className='bg-white w-90 h-max p-10 rounded-2xl flex flex-col gap-5 items-center '>
       <h1 className='font-semibold text-lg'>Game History</h1>
-      <GameStats>
-        <GameStatsCard label="X Wins" value={0} />
-        <GameStatsCard label="O Wins" value={0} />
-        <GameStatsCard label="Draws" value={0} />
-        <GameStatsCard label="Total Games" value={0} />
+      {
+        !isClient ?
+        <div>Loading...</div> :
+          <GameStats>
+          <GameStatsCard label="X Wins" value={gameStats.xScore} />
+          <GameStatsCard label="O Wins" value={gameStats.oScore} />
+          <GameStatsCard label="Draws" value={gameStats.draw} />
+          <GameStatsCard label="Total Games" value={gameStats.totalGames} />
       </GameStats>
+      }
     </div>
   )
 }
